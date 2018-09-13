09e6c3d734fb1396dcd4c178b707e8cb31336486
Anastasia Karpenko <morganaloca@gmail.com>
2018-09-13 10:30:29 +0300

    feature done

--- app/src/main/java/com/example/karpena2/recyclertest/MainActivity.java
+++ app/src/main/java/com/example/karpena2/recyclertest/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Add a Fragment with RecyclerView
+    // Add a Fragment with RecyclerView | done
     // Add an Adapter, Holder and Generator of Data
     // Add data refresh and state of error
     // Add data loading from the contacts app
