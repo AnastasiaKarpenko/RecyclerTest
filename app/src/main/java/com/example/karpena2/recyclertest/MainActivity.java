45140366131e908feaa7a8f07c2bad0589ca4291
Anastasia Karpenko <morganaloca@gmail.com>
2018-09-13 10:08:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/karpena2/recyclertest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.karpena2.recyclertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Add a Fragment with RecyclerView
+    // Add an Adapter, Holder and Generator of Data
+    // Add data refresh and state of error
+    // Add data loading from the contacts app
+    // Handle on click
+    // Add decorators
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
