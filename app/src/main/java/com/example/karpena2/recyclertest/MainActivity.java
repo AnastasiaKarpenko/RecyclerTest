938148e4e2d0974a262c9dfb38816996b7f9f078
Anastasia Karpenko <morganaloca@gmail.com>
2018-09-14 17:45:10 +0300

    Added a menu item to cancel call request. Menu item does nothing without a call request being sent.

--- app/src/main/java/com/example/karpena2/recyclertest/MainActivity.java
+++ app/src/main/java/com/example/karpena2/recyclertest/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.karpena2.recyclertest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ContactsAdapter.OnItemClickListener, LoaderManager.LoaderCallbacks<String> {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private String ID = "ID";
 
     @Override
@@ -22,6 +28,29 @@ public class MainActivity extends AppCompatActivity implements ContactsAdapter.O
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, RecyclerFragment.newInstance())
                     .commit();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_cancel_request:
+                if(getSupportLoaderManager().hasRunningLoaders()) {
+                    getSupportLoaderManager().destroyLoader(0);
+                    Log.d(TAG, "loader destroyed");
+                }
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
--- app/src/main/java/com/example/karpena2/recyclertest/PhoneNumberLoader.java
+++ app/src/main/java/com/example/karpena2/recyclertest/PhoneNumberLoader.java
@@ -7,6 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.AsyncTaskLoader;
 
+import java.util.concurrent.TimeUnit;
+
 public class PhoneNumberLoader extends AsyncTaskLoader<String> {
     private String mContactId;
     private Context mContext;
@@ -22,6 +24,14 @@ public class PhoneNumberLoader extends AsyncTaskLoader<String> {
     @Nullable
     @Override
     public String loadInBackground() {
+
+        // 2 seconds delay
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Cursor cursor = mContext.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
                 ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
